7df489f0198ea3cc09ec1df2c152b8ad430c5f71
Oleksandr <zadorozhniy3@icloud.com>
2023-04-07 17:24:17 +0300

    create models & migrations

---
+++ app/Models/Essence.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Essence extends Model
+{
+    use HasFactory;
+}
